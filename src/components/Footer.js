359607dc68168e6e8a7efc93487b3c76d9de98ea
livvstrongg <livyhawk@me.com>
2022-06-06 10:21:48 -0500

    Added text to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
 
-function Footer() {
+const Footer = () => {
   return (
-    <div></div>
+    <footer>
+      <Container>
+        <Row>
+          <Col className='text-center py-3'>2022</Col>
+        </Row>
+      </Container>
+    </footer>
   )
 }
 
