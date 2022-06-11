8e6b0a665dea78e0350daf6c7807bc854c214735
livvstrongg <livyhawk@me.com>
2022-06-10 19:00:31 -0500

    trying to do cart function

--- src/redux/reducer/handleCart.js
+++ src/redux/reducer/handleCart.js
@@ -4,8 +4,33 @@ const handleCart = (state = cart, action) => {
     const product = action.payload;
     switch (action.type) {
         case ADDITEM:
+            //to see if product exist
             const exist = state.find((x) => x.id === product.id);
+            if(exist){
+                //increasing quantity
+                return state.map((x) =>
+                x.id === product.id ? {...x, qty: x.qty + 1} : x 
+                );
+            }else{
+                const product = action.payload;
+                return[
+                    ...state,
+                    {
+                        ...product,
+                        qty: 1,
+                    }
+                ]
+            }
             break;
+
+            case "DELITEM":
+                const exist1 = state.find((x) => x.id === product.id);
+                if(exist1.qty === 1){
+                    return state.filter((x) => x.id !== exist1.id);
+                }else{
+                    return state.map((x) => x.id === product.id ? {...x, qty: x.qty-1} : x)
+                }
+                break;
         
         default: break;
     }
